54ed0bf87ee04e90ab18518df6fa4b244e40fda0
Andrey Bochkov <bocandrei7@icloud.com>
2020-05-04 19:08:47 +0200

    Добавлен метод сохранения результатов прохождения опросов (пока без проверки валидности)

--- install/components/savmaxru/forms.viewform/class.php
+++ install/components/savmaxru/forms.viewform/class.php
@@ -9,11 +9,47 @@ use Bitrix\Main\Localization\Loc;
 \Bitrix\Main\Loader::includeModule('savmaxru.forms');
 
 
-class CSavmaxruFormsView extends CBitrixComponent
+class CSavmaxruFormsView extends CBitrixComponent implements Controllerable
 {
 	public function executeComponent()
 	{
 		$this->includeComponentTemplate();
 	}
 
+	public function configureActions()
+	{
+		return array();
+	}
+
+	public function saveResultAction($result)
+	{
+		$answerResultTable = new \Savmaxru\Forms\Model\AnswerResultTable();
+		$answerToQuestionTable = new \Savmaxru\Forms\Model\AnswerToQuestionTable();
+		$answerOptionTable = new \Savmaxru\Forms\Model\AnswerOptionTable();
+
+		global $USER;
+		$idUser = $USER->GetID();
+
+		$maxIdResult = $answerResultTable->getMaxIdResult();
+		$idResult = $maxIdResult + 1;
+
+		$answerResultTable->saveAnswerResult($idResult, $result["ID"], $idUser);
+
+		foreach ($result["questions"] as $question)
+		{
+			$answerToQuestionTable->saveAnswerToQuestion($question["ID"], $idResult);
+
+			if (array_key_exists('userValue', $question["options"]))
+			{
+				$answerOptionTable->saveOptionAnswer($question["ID"], 0, $question["options"]["userValue"]);
+			}
+			else
+			{
+				foreach ($question["options"] as $option)
+				{
+					$answerOptionTable->saveOptionAnswer($question["ID"], $option, '');
+				}
+			}
+		}
+	}
 }
